0d9f4cea7084360f747e791f38cb2df065b043a4
bopad <borisdelord@proton.me>
2025-01-21 16:35:24 +0100

    add font and color var

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,18 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backMad: "#F7F2EC",
+        redMad: "#8C1C13",
+        textMad: "#252525",
+        encartMad: "#BF4342",
+      },
+      fontFamily: {
+        titleMad: ["Arbutus Slab", "serif"],
+        bodyMad: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
