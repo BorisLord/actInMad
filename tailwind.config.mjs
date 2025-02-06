83c59e85fc2b63753648e8bf90acfec25fb0a7d1
bopad <borisdelord@proton.me>
2025-02-06 11:28:43 +0100

    migrate tailwind.config to global.css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,21 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
 
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        madBack: "#F5ECE1",
-        madRed: "#8C1C13",
-        madText: "#252525",
-        madEncart: "#BF4342",
-      },
-      fontFamily: {
-        madTitleFont: ["Arbutus Slab", "serif"],
-        madBodyFont: ["Inter", "sans-serif"],
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/forms")],
-};
+// export default {
+//   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         madBack: "#F5ECE1",
+//         madRed: "#8C1C13",
+//         madText: "#252525",
+//         madEncart: "#BF4342",
+//       },
+//       fontFamily: {
+//         madTitleFont: ["Arbutus Slab", "serif"],
+//         madBodyFont: ["Inter", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/forms")],
+// };
 // "@tailwindcss/forms": "^0.5.10",
