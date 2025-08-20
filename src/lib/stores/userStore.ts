a58cc9765b363bf0927d7ef45af4d2ef1302a7d2
boPad <borisdelord@proton.me>
2025-08-20 21:28:53 +0200

    userStore upgrade

--- src/lib/stores/userStore.ts
+++ src/lib/stores/userStore.ts
@@ -1,26 +1,40 @@
-import { persistentMap } from "@nanostores/persistent";
+import { map } from "nanostores";
 
 export type User = {
   token: string;
   id: string;
   email: string;
   verified: string;
+  firstName?: string;
+  lastName?: string;
+  phone?: string;
+  theaterLevel?: "Debutant" | "Intermediaire" | "Confirme" | undefined;
+  profileCompleted?: string;
 };
 
-export const $user = persistentMap<User>("user:", {
+export const $user = map<User>({
   token: "",
   id: "",
   email: "",
   verified: "",
+  firstName: undefined,
+  lastName: undefined,
+  phone: undefined,
+  theaterLevel: undefined,
+  profileCompleted: undefined,
 });
 
 export function setUser(authData: any) {
-  // type pocketbase
   $user.set({
     token: authData.token,
     id: authData.record.id,
     email: authData.record.email,
     verified: authData.record.verified,
+    firstName: authData.record.firstName,
+    lastName: authData.record.lastName,
+    phone: authData.record.phone,
+    theaterLevel: authData.record.theaterLevel,
+    profileCompleted: authData.record.profileCompleted,
   });
 }
 
