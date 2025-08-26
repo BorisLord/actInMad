e2791c49357b261d8066f797d1a1a62a2f607460
boPad <borisdelord@proton.me>
2025-08-26 08:13:23 +0200

    add prettier-plugin-tailwindcss

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,9 +1,15 @@
-/** @type {import("prettier").Config} */
+// /** @type {import("prettier").Config} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-  plugins: ["prettier-plugin-astro", "@trivago/prettier-plugin-sort-imports"],
+  plugins: [
+    "prettier-plugin-astro",
+    "@trivago/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
   importOrder: ["<THIRD_PARTY_MODULES>", "^@/(.*)$", "^[./]"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
+  tailwindStylesheet: "./styles/global.css",
   overrides: [
     {
       files: "*.astro",
